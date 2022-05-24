215f8d6718cd58f5bb3dd9100c2964346478a55c
Khalitov62 <tu7m5rk73code.edu.az>
2022-05-24 13:56:59 +0400

    added

---
+++ AP204_Pronia/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using AP204_Pronia.DAL;
+using AP204_Pronia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AP204_Pronia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+    }
+}
